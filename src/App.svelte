50e357341654841abf414b839a92f6b06f571cd2
hmilly <milstowers@gmail.com>
2022-03-21 19:53:29 +0000

    first update

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  //   let first = "H";
+  //   let last = "m"
+  //   $: name = first  + last
+</script>
+
+<main>
+  <!-- <h1>Hi {name}!</h1>
+  <p>
+
+ 
+  </p> -->
+</main>
+
+<style>
+  main {
+    text-align: center;
+	height: 100vh;
+
+    margin: 0 auto;
+	background-color: navy;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
